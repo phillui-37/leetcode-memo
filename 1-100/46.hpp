6e61816ae7db0862a47d174f51c41c3f54fded1e
Phil Lui <phillui37@gmail.com>
2023-04-25 22:44:37 +0800

    46 done

---
+++ 1-100/46.hpp
@@ -0,0 +1,37 @@
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    /**
+     * this can be done by extract one entry of variable
+     * then call self recurisivly providing sublist except the extracted one until the variable is empty
+    */
+    vector<vector<int>> permute(vector<int>& nums)
+    {
+        if (nums.size() == 0) return {};
+
+        vector<vector<int>> result;
+
+        for (int i=0;i<nums.size();i++)
+        {
+            vector<int> param(nums.cbegin(), nums.cbegin()+i);
+            param.insert(param.end(), nums.cbegin()+i+1,nums.cend());
+
+            vector<vector<int>> otherCombinations = permute(param);
+            if (otherCombinations.size() == 0)
+                return {{nums[i]}};
+            for (int j=0;j<otherCombinations.size();j++)
+            {
+                vector<int> row = {nums[i]};
+                row.insert(row.end(), otherCombinations[j].cbegin(), otherCombinations[j].cend());
+                result.push_back(row);
+            }
+        }
+
+        return result;
+    }
+};
+
---
+++ readme.md
@@ -0,0 +1,11 @@
+## introduction
+self leetcode memo, may use different language to solve same issue
+
+## different language standard
+- cpp: c++20
+- rust: rust2018
+
+## done question
+|leetcode#|src code|
+|---|---|
+|46|[46.hpp](1-100/46.hpp)|
